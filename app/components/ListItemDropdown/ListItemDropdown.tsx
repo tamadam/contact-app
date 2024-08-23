27df8761167c19f05d490f4fa562529f15e26d26
tamadam <tamadam9@gmail.com>
2024-08-23 15:00:15 +0200

    Add DELETE call

--- app/components/ListItemDropdown/ListItemDropdown.tsx
+++ app/components/ListItemDropdown/ListItemDropdown.tsx
@@ -6,6 +6,8 @@ import Button from "../Button/Button";
 import Image from "next/image";
 import { useContactForm } from "@/app/providers/ContactForm/ContactFormContext";
 import { ContactType } from "@/app/types";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
 
 interface ListItemDropdownProps {
   contact: ContactType;
@@ -15,6 +17,38 @@ interface ListItemDropdownProps {
 const ListItemDropdown = forwardRef<HTMLUListElement, ListItemDropdownProps>(
   ({ contact, onClick }: ListItemDropdownProps, ref) => {
     const { openDialog } = useContactForm();
+
+    const router = useRouter();
+
+    const handleContactDelete = async () => {
+      try {
+        //setIsLoading(true);
+
+        if (!contact) throw new Error("No contact available");
+
+        const response = await fetch(`/api/contacts/${contact.id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (response.ok) {
+          // delete contact image
+          // TODO
+
+          router.refresh();
+          toast.success("Contact deleted successfully!");
+        } else {
+          throw new Error();
+        }
+      } catch (error) {
+        toast.error("Delete failed.");
+      } finally {
+        //setIsLoading(false);
+        onClick();
+      }
+    };
 
     return (
       <ul ref={ref} className={styles.dropdownWrapper}>
@@ -50,7 +84,7 @@ const ListItemDropdown = forwardRef<HTMLUListElement, ListItemDropdownProps>(
           </Button>
         </li>
         <li className={styles.dropdownItem}>
-          <Button variant="normal">
+          <Button variant="normal" onClick={handleContactDelete}>
             <Image src="/trash.svg" alt="edit" width={24} height={24} />
             <span>Remove</span>
           </Button>
